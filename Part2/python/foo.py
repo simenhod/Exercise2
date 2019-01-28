2da72da63a845206485fc409d1e380fd4e0a169e
Bendik <bendikbo@stud.ntnu.no>
2019-01-28 12:54:33 +0100

    fikset pythonkode

--- Part2/python/foo.py
+++ Part2/python/foo.py
@@ -1,30 +1,40 @@
 # Python 3.3.3 and 2.7.6
 # python fo.py
 
-from threading import Thread
+import threading
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
+lock = threading.Lock()
+
 
 def incrementingFunction():
     global i
-    # TODO: increment i 1_000_000 times
+    lock.acquire()
+    for j in range(1000000):
+        i += 1
+    lock.release()
+
 
 def decrementingFunction():
     global i
-    # TODO: decrement i 1_000_000 times
+    lock.acquire()
+    for k in range(1000000):
+        i -= 1
+    lock.release()
 
 
 
 def main():
     global i
 
-    incrementing = Thread(target = incrementingFunction, args = (),)
-    decrementing = Thread(target = decrementingFunction, args = (),)
+    incrementing = threading.Thread(target = incrementingFunction, args = (),)
+    decrementing = threading.Thread(target = decrementingFunction, args = (),)
     
-    # TODO: Start both threads
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
